77bf4ed41418dcdab0cfacb5b1840634cbb9a951
dianestar <dianestar@naver.com>
2022-02-13 18:33:48 +0900

    [fix] header dogpaw 로고 추가, css vh 값 및 hover 선택자 수정

--- src/Components/Shared/Header.js
+++ src/Components/Shared/Header.js
@@ -1,15 +1,17 @@
 import logo from '../../Sources/logo.png';
+import dogpaw from '../../Sources/dogpaw.png'
 import styles from './Header.module.css';
 
 function Header() {
     return (
         <div className={styles.header}>
             <div className={styles[`left-tab`]}>
-                <img className={styles.logo} src={logo} alt="logo"/>
-                <span className={styles.title}>너와 나의 밸런스 지수 test</span>
+                <img className={styles.image} src={logo} alt="Balance Game Logo"/>
+                <span className={styles.text}>너와 나의 밸런스 지수 test</span>
             </div>
             <div className={styles[`right-tab`]}>
-                <span className={styles.developer}> Made by DogPaw2</span>
+                <span className={styles.text}> Made by DogPaw2</span>
+                <img className={styles.image} src={dogpaw} alt="Dogpaw Logo"></img>
             </div>
         </div>
     );
