ada86c24415715d27314fc0cd200740c5f7b3394
dianestar <dianestar@naver.com>
2022-06-20 15:08:40 +0900

    [fix] Modify cracked text in Result page

--- src/Pages/Result.js
+++ src/Pages/Result.js
@@ -22,63 +22,60 @@ const Result = () => {
     return (
         <Layout isHeaderOn={true}>
             <div className={styles.result}>
-                <p className={styles.title}>{result.hostName} 밸런스게임 {result.guestName}의 결과</p>
+                <p className={styles.title}>{result.hostName}님의 밸런스게임 <br/> {result.guestName}님의 결과</p>
                 <p>
-                    <span className={`${styles.blue} ${styles[`sub-title`]}`}>{result.score}개</span>
+                    <span className={`${styles.pink} ${styles[`sub-title`]}`}>{result.score}개</span>
                     <span className={styles[`sub-title`]}>/</span>
-                    <span className={`${styles.pink} ${styles[`sub-title`]}`}>{result.questions.length}문제 중</span>
+                    <span className={`${styles.blue} ${styles[`sub-title`]}`}>{result.questions.length}문제 중</span>
                 </p>
                 <div className={styles.table}>
                     <div className={styles.col}>
-                        <p className={styles.header}>문제</p>
+                        <span className={styles.header}>문제</span>
                         {result.questions.map((value, index) => {
                             return (
-                                <div key={index}>
+                                <div key={index} className={styles.square}>
                                     <div className={styles.question}>{value.firstOption}</div>
                                     <div className={styles.question}>{value.secondOption}</div>
-                                    <hr />
                                 </div>
                             );
                         })}
                     </div>
                     <div className={styles.col}>
-                        <p className={styles.header}>{result.hostName}</p>
+                        <span className={styles.header}>{result.hostName}</span>
                         {result.hostAnswers.map((value, index) => {
                             return (
-                                <div key={index}>
+                                <div key={index} className={styles.square}>
                                     {value === 0 ?
                                     <>
-                                        <div className={styles.pink}>O</div>
-                                        <div className={styles.hidden}>O</div>
+                                        <div className={`${styles.pink} ${styles.circle}`}>O</div>
+                                        <div className={`${styles.hidden} ${styles.circle}`}>O</div>
                                     </>
                                     :
                                     <>
-                                        <div className={styles.hidden}>O</div>
-                                        <div className={styles.pink}>O</div>
+                                        <div className={`${styles.hidden} ${styles.circle}`}>O</div>
+                                        <div className={`${styles.blue} ${styles.circle}`}>O</div>
                                     </>
                                     }
-                                    <hr />
                                 </div>
                             );
                         })}
                     </div>
                     <div className={styles.col}>
-                        <p className={styles.header}>{result.guestName}</p>
+                        <span className={styles.header}>{result.guestName}</span>
                         {result.guestAnswers.map((value, index) => {
                             return (
-                                <div key={index}>
+                                <div key={index} className={styles.square}>
                                     {value ===  0 ?
                                     <>
-                                        <div className={styles.blue}>O</div>
-                                        <div className={styles.hidden}>O</div>
+                                        <div className={`${styles.pink} ${styles.circle}`}>O</div>
+                                        <div className={`${styles.hidden} ${styles.circle}`}>O</div>
                                     </>
                                     :
                                     <>
-                                        <div className={styles.hidden}>O</div>
-                                        <div className={styles.blue}>O</div>
+                                        <div className={`${styles.hidden} ${styles.circle}`}>O</div>
+                                        <div className={`${styles.blue} ${styles.circle}`}>O</div>
                                     </>
                                     }
-                                    <hr />
                                 </div>
                             );
                         })}
